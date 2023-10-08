590d94c6c73700725e15cf3ee6810046f1d74612
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-08 19:34:52 +0600

    Implementing two methods in class

--- src/ConcreteComponentLegs.java
+++ src/ConcreteComponentLegs.java
@@ -1,2 +1,12 @@
 public class ConcreteComponentLegs implements Modification{
+
+    @Override
+    public String getDescription() {
+        return "Basic legs with standard functionality";
+    }
+
+    @Override
+    public double getCost() {
+        return 0.0;
+    }
 }
