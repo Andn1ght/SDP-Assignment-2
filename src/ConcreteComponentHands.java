d129ff92364673189f1eda63a4180108af9ba544
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-08 19:32:53 +0600

    Creating new class that will be concrete component

---
+++ src/ConcreteComponentHands.java
@@ -0,0 +1,12 @@
+public class ConcreteComponentHands implements Modification{
+
+    @Override
+    public String getDescription() {
+        return "Basic hands with standard functionality";
+    }
+
+    @Override
+    public double getCost() {
+        return 0.0;
+    }
+}
